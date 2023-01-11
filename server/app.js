c635a9320608a52e55246e64543f2139030d0bc2
Atika72 <atika72rahman@gmail.com>
2023-01-11 19:40:23 +0600

    2nd commit

--- server/app.js
+++ server/app.js
@@ -18,9 +18,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
     console.log("db connected")
 })
 .catch((err)=>{
-    console.log(err.toString());
-    console.log("hi");
-    console.log("hoise");
+    console.log(err.toString())
 })
 
 //api route
